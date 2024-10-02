0831f05fdc3438a36627075a36147643a198adb6
Lukas Müller <WookieFPV@users.noreply.github.com>
2024-10-02 21:49:43 +0200

    add token auth, add example api to serve static images

--- src/env.ts
+++ src/env.ts
@@ -7,6 +7,7 @@ const envSchema = z.object({
     TS3_NICKNAME: z.string().optional(),
     TS3_USERNAME: z.string(),
     TS3_USER_CID: z.string().optional(),
+    API_SECRET: z.string(),
 }).catch((e) => {
     console.log("Missing env vars!")
     console.log(e.error);
@@ -21,4 +22,5 @@ export const env = envSchema.parse({
     TS3_NICKNAME: process.env.TS3_NICKNAME,
     TS3_USERNAME: process.env.TS3_USERNAME,
     TS3_USER_CID: process.env.TS3_USER_CID,
+    API_SECRET: process.env.API_SECRET,
 })
--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,15 @@ import {apiTsChannels} from "./api/ts/channels/root.ts";
 import {apiTsUsersById} from "./api/ts/users/ById.ts";
 import {apiTsUsersPoke} from "./api/ts/users/poke.ts";
 import {apiTsUsers} from "~/api/ts/users/root.ts";
+import type {JwtVariables} from 'hono/jwt'
+import {bearerAuth} from 'hono/bearer-auth'
+import {env} from "~/env.ts";
+import {serveStatic} from "hono/bun";
 
-const app = new Hono().basePath('/api')
+// this is against the Best Practices, but I will keep it this way for now: https://hono.dev/docs/guides/best-practices#don-t-make-controllers-when-possible
+const app = new Hono<{ Variables: JwtVariables }>().basePath('/api')
+
+app.use('/ts/*', bearerAuth({token: env.API_SECRET}))
 
 app.get('/ts/channels', apiTsChannels)
 
@@ -22,5 +29,9 @@ app.get('/sleep/:time', (c) => apiSleep(c, c.req.param('time')))
 
 app.get('/sleep', (c) => apiSleep(c, "1000"))
 
+app.use('/wookie', serveStatic({path: 'src/api/demo/wookie.jpg'}))
 
+app.get('/debug', (c) => {
+    return c.json({"foo": "bar"})
+})
 export default app
