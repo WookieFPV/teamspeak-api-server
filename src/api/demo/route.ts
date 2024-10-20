1cb20b5d0936c34a3eb89ab573678e326ede1b97
Lukas Müller <WookieFPV@users.noreply.github.com>
2024-10-20 18:02:15 +0200

    improve sleep time

--- src/api/demo/route.ts
+++ src/api/demo/route.ts
@@ -1,13 +1,13 @@
 import { Hono } from 'hono';
 import { apiDemoDebug } from '~/api/demo/debug.ts';
-import { apiDemoSleep, apiDemoSleepTime } from '~/api/demo/sleep.ts';
+import { apiSleep } from '~/api/demo/sleep.ts';
 import { apiDemoWookie } from '~/api/demo/wookie.ts';
 
 export const honoRouteDemo = new Hono();
 
-honoRouteDemo.get('/sleep/:time', apiDemoSleepTime);
+honoRouteDemo.get('/sleep/:time', (c) => apiSleep(c, c.req.param('time')));
 
-honoRouteDemo.get('/sleep', apiDemoSleep);
+honoRouteDemo.get('/sleep', (c) => apiSleep(c, '1000'));
 
 honoRouteDemo.get('/wookie', apiDemoWookie);
 
--- src/api/demo/sleep.ts
+++ src/api/demo/sleep.ts
@@ -2,14 +2,7 @@ import { sleep } from 'bun';
 import type { Context } from 'hono';
 import { HTTPException } from 'hono/http-exception';
 
-export const apiDemoSleepTime = async (c: Context) =>
-  apiSleep(c, c.req.param('time'));
-
-export const apiDemoSleep = async (c: Context) => apiSleep(c, '1000');
-
 export const apiSleep = async (c: Context, time: string) => {
-  if (!time) throw new HTTPException(400, { message: 'time not set' });
-
   const timeInt = Number.parseInt(time);
   if (Number.isNaN(timeInt))
     throw new HTTPException(400, { message: 'time is not a number' });
